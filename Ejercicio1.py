413d833e2f8f65731c5c78f5cb1f3820d08c950c
danielf059 <161541379+danielf059@users.noreply.github.com>
2024-03-12 20:28:16 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,21 @@
+#Primer ejercicio
+operacion = float((3 + 2) /2.5 ) **2
+print(operacion)
+
+#Segundo ejercicio
+payasos = (112*23)
+muñecas = (75*54)
+peso_total = payasos + muñecas
+
+print(f"El peso total en gramos es: {peso_total}")
+
+#Tercer ejercicio
+cadena = "Te quiero solo como amigo"
+print(f"Los dos primeros caracteres son: {cadena [0:2]}")
+print(f"Los tres ultimos caracteres son: {cadena[-3:]}")
+print(f"Cada dos caracteres: {cadena[::2]}")
+print(f"Sentido inverso: {cadena[::-1]}")
+print(f"Sentido normal e inverso: {cadena[0:]} {cadena[::-1]}")
+
+#Cuarto ejercicio
+cadena =replace(,)"Separado"
